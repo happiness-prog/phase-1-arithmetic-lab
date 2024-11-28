bb0cf98e1b40f5164478f28f70d5a23dc93ce906
Happiness Maina <mainahappiness361@gmail.com>
2024-11-28 13:56:04 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply= (num1 *num2)
+
+
+let x=10;
+let y=7;
+let z=20;
+let max =Math.max(x, y, z);
+
+
+
+let random =Math.floor(Math.random()*20)+1;
+
+
+let num3=32;
+let num4=7;
+let mod =(num3 % num4);
